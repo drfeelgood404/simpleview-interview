6dd5bbaa40d09627273e1c55ed99ccd45d02c7a8
Arin Khatua <arin.khatua@icloud.com>
2024-02-04 22:21:05 +0530

    Added MySQL Database

---
+++ simpleview_backup.sql
@@ -0,0 +1,77 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for macos14 (arm64)
+--
+-- Host: localhost    Database: simpleview
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Departments`
+--
+
+DROP TABLE IF EXISTS `Departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Departments` (
+  `DepartmentID` int NOT NULL,
+  `DepartmentName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`DepartmentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Departments`
+--
+
+LOCK TABLES `Departments` WRITE;
+/*!40000 ALTER TABLE `Departments` DISABLE KEYS */;
+INSERT INTO `Departments` VALUES (1,'HR'),(2,'Engineering'),(3,'Marketing'),(4,'Finance');
+/*!40000 ALTER TABLE `Departments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Employees`
+--
+
+DROP TABLE IF EXISTS `Employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Employees` (
+  `EmployeeID` int NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Salary` int DEFAULT NULL,
+  `DepartmentID` int DEFAULT NULL,
+  PRIMARY KEY (`EmployeeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Employees`
+--
+
+LOCK TABLES `Employees` WRITE;
+/*!40000 ALTER TABLE `Employees` DISABLE KEYS */;
+INSERT INTO `Employees` VALUES (1,'Alice',70000,1),(2,'Bob',80000,1),(3,'Charlie',55000,2),(4,'David',45000,2),(5,'Eve',75000,2),(6,'Frank',65000,3),(7,'Grace',60000,3),(8,'Hannah',85000,3),(9,'Ivan',90000,4),(10,'John',50000,4);
+/*!40000 ALTER TABLE `Employees` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-04 22:20:34
